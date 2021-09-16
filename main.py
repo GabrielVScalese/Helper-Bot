bbfc8f1fd4d0918bf7900c8ad94959fcce96b8f7
gabriel scalese <cc19171@g.unicamp.br>
2021-09-16 15:58:17 -0300

    Update

--- main.py
+++ main.py
@@ -39,12 +39,21 @@ async def status(ctx):
 
   msg = await client.wait_for("message", check=lambda message: message.author == ctx.author)
 
+  description = ''
   if msg == 'off':
     on = False
-  else:
+    description = f'Meu status agora é **off**'
+  elif msg == 'on':
     on = True
+    description = f'Meu status agora é **on**'
+  else:
+    description = f'Meu status não foi modificado'
 
-  await ctx.send('Status modificado  ✅')
+  embed = discord.Embed(title='Comunicado da Monitoria', description=description, color=discord.Color.blue())
+
+  embed.set_author(name='Eve', icon_url=str(client.user.avatar_url))
+
+  await ctx.send(embed=embed)
 
 @client.command()
 async def hoje (ctx):
@@ -58,53 +67,54 @@ async def on_ready():
 
 @tasks.loop(seconds=1)
 async def called_once_a_day():
-  try:
-    for channel in channels:
-      vc = client.get_channel(id=channel['id'])
-      currentUsers = vc.members
+  if on == True:
+    try:
+      for channel in channels:
+        vc = client.get_channel(id=channel['id'])
+        currentUsers = vc.members
 
-      userNoticeController.updateNotices(channel['id'], currentUsers)
+        userNoticeController.updateNotices(channel['id'], currentUsers)
 
-      if len(currentUsers) == 0:
-        continue
+        if len(currentUsers) == 0:
+          continue
 
-      helper = await client.fetch_user(channel['owner']['id'])
-      helperName = channel['owner']['name']
+        helper = await client.fetch_user(channel['owner']['id'])
+        helperName = channel['owner']['name']
 
-      if Schedules.verifyNow(channel['owner']['id']):
-        if not existsInChannel(channel['owner']['id'], currentUsers):
+        if Schedules.verifyNow(channel['owner']['id']):
+          if not existsInChannel(channel['owner']['id'], currentUsers):
+            for user in currentUsers:
+              if user.id != teacherId:
+                if not userNoticeController.existsUser(user.id, channel['id']):
+                  await Sender.sendEmbedToHelper(client, helper, 'Comunicado da Monitoria', f'Olá **{helperName}**! Novos alunos estão em seu canal.', currentUsers)
+            
+                  await Sender.sendEmbedToUser(client, user, 'Comunicado da Monitoria', f'Olá **{user.display_name}**! Já entrei em contato com o **monitor {helperName}** e logo ele estará aqui.', helper.avatar_url)
+
+                  userNoticeController.addNotice(user.id, channel['id'])
+
+          else:
+            if usersChannelController.lenUsersChannel(channel['id']) != len(currentUsers):
+              usersChannelController.addUsersChannel(channel['id'], currentUsers)
+
+              usersChannel = usersChannelController.findUsersChannel(channel['id'])
+              
+              await Sender.sendEmbedToHelper(client, helper, 'Comunicado da Monitoria', f'Olá **{helperName}**! A lista de alunos foi atualizada.', usersChannel)
+
+        else:
           for user in currentUsers:
             if user.id != teacherId:
               if not userNoticeController.existsUser(user.id, channel['id']):
-                await Sender.sendEmbedToHelper(client, helper, 'Comunicado da Monitoria', f'Olá **{helperName}**! Novos alunos estão em seu canal.', currentUsers)
-          
-                await Sender.sendEmbedToUser(client, user, 'Comunicado da Monitoria', f'Olá **{user.display_name}**! Já entrei em contato com o **monitor {helperName}** e logo ele estará aqui.', helper.avatar_url)
+                if user.id != channel['owner']['id']:
+                  schedulesGroup = Schedules.findByHelper(helper)
 
-                userNoticeController.addNotice(user.id, channel['id'])
+                  await Sender.sendEmbedToUser(client, user, 'Comunicado da Monitoria', f'Infelizmente, o **monitor {helperName}** não atende monitoria neste momento. Confira abaixo todos **horários** desse monitor:', thumbnail = helper.avatar_url)
 
-        else:
-          if usersChannelController.lenUsersChannel(channel['id']) != len(currentUsers):
-            usersChannelController.addUsersChannel(channel['id'], currentUsers)
+                  await Sender.sendMessageToUser(user, f'```{schedulesGroup}```')
 
-            usersChannel = usersChannelController.findUsersChannel(channel['id'])
-            
-            await Sender.sendEmbedToHelper(client, helper, 'Comunicado da Monitoria', f'Olá **{helperName}**! A lista de alunos foi atualizada.', usersChannel)
+                  userNoticeController.addNotice(user.id, channel['id'])
 
-      else:
-        for user in currentUsers:
-          if user.id != teacherId:
-            if not userNoticeController.existsUser(user.id, channel['id']):
-              if user.id != channel['owner']['id']:
-                schedulesGroup = Schedules.findByHelper(helper)
-
-                await Sender.sendEmbedToUser(client, user, 'Comunicado da Monitoria', f'Infelizmente, o **monitor {helperName}** não atende monitoria neste momento. Confira abaixo todos **horários** desse monitor:', thumbnail = helper.avatar_url)
-
-                await Sender.sendMessageToUser(user, f'```{schedulesGroup}```')
-
-                userNoticeController.addNotice(user.id, channel['id'])
-
-  except Exception as e:
-    print(e)
+    except Exception as e:
+      print(e)
     
 @called_once_a_day.before_loop
 async def before():
